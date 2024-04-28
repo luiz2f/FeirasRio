47e8cc0ba5f78251246547b69766d977396bbb8a
jacarezin <luizfellipe230@gmail.com>
2024-04-28 20:55:47 -0300

    checklist - date

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
 import WeekCalendar from "./WeekCalendar";
+const diasDaSemana = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 function Header() {
+  const [horaAtual, setHoraAtual] = useState("");
+
+  function formatarHora(date) {
+    const diaSemana = diasDaSemana[date.getDay()];
+    const hora = date.getHours();
+    const minutos = date.getMinutes();
+    return `${diaSemana} - ${hora}:${minutos < 10 ? "0" : ""}${minutos}`;
+  }
+
+  useEffect(() => {
+    const atualizarHora = () => {
+      const dataAtual = new Date();
+      const horaFormatada = formatarHora(dataAtual);
+      setHoraAtual(horaFormatada);
+    };
+    atualizarHora();
+    const intervalId = setInterval(atualizarHora);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="header">
       <div className="flexcontainer">
@@ -10,7 +40,7 @@ function Header() {
           </div>
           <div className="textheader">
             <div className="titulo">FEIRAS DE RUA</div>
-            <div className="data">Segunda-Feira - 18:08</div>
+            <div className="data">{horaAtual}</div>
           </div>
         </div>
         <div className="meio">
